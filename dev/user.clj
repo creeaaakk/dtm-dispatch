a54810f74ebf0a7448af5cca346fb73fc3cb7620
Brian Rowe <briprowe@yahoo.com>
2013-07-01 08:02:14 -0400

    Rudimentary dispatch seems to be working.

--- dev/user.clj
+++ dev/user.clj
@@ -1,6 +1,49 @@
 (ns user
   (:use [clojure repl pprint]
         [clojure.tools.namespace.repl :only [refresh]]
+        criterium.core
         com.creeaaakk.dtm-dispatch)
-  (:require dev))
+  (:require dev)
+  (:import [java.util.concurrent LinkedBlockingQueue]))
 
+(def job-count (atom {}))
+
+(defn new-user-job
+  [key]
+  #(swap! job-count update-in [:new-user-job key] (fnil inc 0)))
+
+(defn removed-user-job
+  [key]
+  #(swap! job-count update-in [:removed-user-job key] (fnil inc 0)))
+
+(defn notify-contacts-job
+  [key]
+  #(swap! job-count update-in [:notify-contacts-job key] (fnil inc 0)))
+
+(def dsp1 (dispatch-fn [_ :user/username _ true] (bound-fn* (new-user-job :dsp1))
+                      [_ :user/username _ false] (bound-fn* (removed-user-job :dsp1))
+                      [_ (:or :user/phone-number
+                              :device/phone-number
+                              :contact/phone-number
+                              :phone-number/number) _ _] (bound-fn* (notify-contacts-job :dsp1))))
+
+(def dsp2 (dispatch-fn [_ :user/username _ true] (bound-fn* (new-user-job :dsp2))
+                      [_ :user/username _ false] (bound-fn* (removed-user-job :dsp2))
+                      [_ (:or :user/phone-number
+                              :device/phone-number
+                              :contact/phone-number
+                              :phone-number/number) _ _] (bound-fn* (notify-contacts-job :dsp2))))
+
+(def q (LinkedBlockingQueue. (repeatedly 100000 #(rand-nth [[1 :user/username 10 false]
+                                                           [1 :user/username 10 true]
+                                                           [1 :user/phone-number 10 true]
+                                                           'foo]))))
+
+(def e (executor dsp1 q))
+
+(defn foo
+  []
+  (set-dispatch-table e dsp1)
+  (start e)
+  (Thread/sleep 100)
+  (set-dispatch-table e dsp2))
--- src/com/creeaaakk/dtm_dispatch.clj
+++ src/com/creeaaakk/dtm_dispatch.clj
@@ -1,5 +1,37 @@
 (ns com.creeaaakk.dtm-dispatch
-  (:require [clojure.core.match :refer [match]]))
+  (:require [clojure.core.match :refer [match]])
+  (:import [java.util.concurrent
+            ThreadPoolExecutor ThreadPoolExecutor$DiscardPolicy
+            TimeUnit SynchronousQueue BlockingQueue]))
+
+(defprotocol IDispatch
+  (set-dispatch-table [this table])
+  (dispatch [this args]))
+
+(defprotocol IDaemon
+  (start [this])
+  (stop [this]))
+
+(declare pumping-loop)
+
+(deftype DispatchingExecutor
+    [dispatch-table executor ^BlockingQueue txn-queue ^BlockingQueue work-queue stop? pumping-thread]
+
+  IDaemon
+  (start [this]
+    (if (nil? @pumping-thread)
+      (do (reset! pumping-thread (Thread. (pumping-loop txn-queue this stop? executor) "pumping-thread"))
+          (.start @pumping-thread))
+      (throw (ex-info "Tried to start non-new pumping-thread." {:thread-state (.getState pumping-thread)}))))
+  (stop [_]
+    (reset! stop? true)
+    (.shutdown executor))
+    
+  IDispatch
+  (set-dispatch-table [_ table]
+    (reset! dispatch-table table))
+  (dispatch [_ args]
+    (@dispatch-table args)))
 
 (defn resolve-handler
   [ns env handler]
@@ -22,4 +54,30 @@
   [& handlers]
   (make-dispatch-fn &env (partition 2 handlers)))
 
+(defn pumping-loop
+  [txn-queue dispatch-table stop? executor]
+  (fn [] (loop [txn (.poll txn-queue 100 TimeUnit/MILLISECONDS)]
+          (when-not @stop?
+            (if-let [handler (dispatch dispatch-table txn)]
+              (.execute executor handler))
+            (recur (.poll txn-queue 100 TimeUnit/MILLISECONDS))))))
+
+(defn executor
+  "Given a sequence of txn handlers and a txn-queue, return a
+  ThreadPoolExecutor that will run the appropriate handler for each
+  txn in the queue."
+  ([txn-queue]
+     (executor (dispatch-fn)))
+  ([dispatch txn-queue]
+     (executor dispatch txn-queue (SynchronousQueue.)))
+  ([dispatch txn-queue work-queue]
+     (let [num-procs (.availableProcessors (Runtime/getRuntime))]
+       (executor (ThreadPoolExecutor. num-procs (* 2 num-procs)
+                                      500 TimeUnit/MILLISECONDS
+                                      work-queue
+                                      (ThreadPoolExecutor$DiscardPolicy.))
+                 dispatch
+                 txn-queue work-queue)))
+  ([executor dispatch txn-queue work-queue]
+     (->DispatchingExecutor (atom dispatch) executor txn-queue work-queue (atom false) (atom nil))))
 
